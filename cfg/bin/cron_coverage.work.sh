8c1b05441a71867f8e07fdb49badd93703a3db78
dm <dmitriimakhov@gmail.com>
2015-12-04 14:07:36 +0300

    MOBI-130 # cron_coverage.sh

---
+++ cfg/bin/cron_coverage.work.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+##
+#   Code coverage generation from Cron
+#
+#   (all placeholders ${...} should be replaced by real values from 'templates.vars.work.json' file
+#    see node 'extra/praxigento_templates_config' in project's 'composer.json')
+##
+
+cd ${LOCAL_ROOT}
+
+git pull
+composer update
+${LOCAL_ROOT}/vendor/bin/phpunit --configuration ${LOCAL_ROOT}/test/coverage/phpunit.dist.xml
