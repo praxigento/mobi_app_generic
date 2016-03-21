567bd788ca9f0365fe0664749011be8b08098cbc
Alex Gusev <alex@flancer64.com>
2016-03-21 17:19:57 +0200

    MOBI-254 - Warehouse-Lot-Qty related structures refactoring

--- deploy/bin/setup.work.sql
+++ deploy/bin/setup.work.sql
@@ -8,3 +8,7 @@ REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = 'America/Los_Angeles',
 -- MOBI-254
 ALTER TABLE ${CFG_DB_PREFIX}cataloginventory_stock_item ADD UNIQUE INDEX CATALOGINVENTORY_STOCK_ITEM_PRODUCT_ID_STOCK_ID (product_id, stock_id);
 ALTER TABLE ${CFG_DB_PREFIX}cataloginventory_stock_item DROP INDEX CATALOGINVENTORY_STOCK_ITEM_PRODUCT_ID_WEBSITE_ID;
+
+ALTER TABLE ${CFG_DB_PREFIX}cataloginventory_stock
+  ADD CONSTRAINT FK_cataloginventory_stock_store_website_website_id FOREIGN KEY (website_id)
+  REFERENCES ${CFG_DB_PREFIX}store_website(website_id) ON DELETE RESTRICT ON UPDATE RESTRICT;
