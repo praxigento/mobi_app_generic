7a9ea8e16249bcf40510724622834e4c70331146
Alex Gusev <alex@flancer64.com>
2016-03-21 16:47:22 +0200

    MOBI-254 - Warehouse-Lot-Qty related structures refactoring

--- deploy/bin/setup.work.sql
+++ deploy/bin/setup.work.sql
@@ -4,3 +4,7 @@
 REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = '1', path ='dev/template/allow_symlink';
 REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = '1', path ='dev/log/active';
 REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = 'America/Los_Angeles', path ='general/locale/timezone';
+
+-- MOBI-254
+ALTER TABLE ${CFG_DB_PREFIX}cataloginventory_stock_item ADD UNIQUE INDEX CATALOGINVENTORY_STOCK_ITEM_PRODUCT_ID_STOCK_ID (product_id, stock_id);
+ALTER TABLE ${CFG_DB_PREFIX}cataloginventory_stock_item DROP INDEX CATALOGINVENTORY_STOCK_ITEM_PRODUCT_ID_WEBSITE_ID;
