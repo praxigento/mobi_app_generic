3b6a667b0d29a2026ff69a71742c44168574428a
Alex Gusev <alex@flancer64.com>
2016-06-08 14:38:46 +0300

    MOBI-304 - Additional store for second warehouse

--- src/Console/Command/Init/Stocks.php
+++ src/Console/Command/Init/Stocks.php
@@ -25,12 +25,21 @@ class Stocks extends Command
     /**#@+
      * IDs for stores (store views in adminhtml terms).
      */
+    const DEF_STOCK_ID_BALTIC = self::DEF_STOCK_ID_DEFAULT;
+    const DEF_STOCK_ID_DEFAULT = 1;
+    const DEF_STOCK_ID_RUSSIAN = 2;
+    /**#@-  */
+
+    /**#@+
+     * IDs for stores (store views in adminhtml terms).
+     */
     const DEF_STORE_ID_ADMIN = 0;
     const DEF_STORE_ID_BALTIC_EN = self::DEF_STORE_ID_DEFAULT;
     const DEF_STORE_ID_BALTIC_RU = 2;
     const DEF_STORE_ID_DEFAULT = 1;
     const DEF_STORE_ID_RUSSIAN_RU = 3;
     /**#@-  */
+
     /**#@+
      * IDs for websites.
      */
@@ -39,35 +48,43 @@ class Stocks extends Command
     /**#@-  */
 
     /** @var  \Magento\Store\Model\Group */
-    private $_groupBaltic;
+    protected $_groupBaltic;
     /** @var  \Magento\Store\Model\Group */
-    private $_groupRussian;
+    protected $_groupRussian;
     /** @var  \Magento\Store\Api\GroupRepositoryInterface */
     protected $_mageRepoGroup;
+    /** @var  \Magento\CatalogInventory\Api\StockRepositoryInterface */
+    protected $_mageRepoStock;
     /** @var  \Magento\Store\Api\StoreRepositoryInterface */
     protected $_mageRepoStore;
     /** @var \Magento\Framework\ObjectManagerInterface */
     protected $_manObj;
     /** @var  \Praxigento\Core\Repo\ITransactionManager */
     protected $_manTrans;
+    /** @var  \Praxigento\Odoo\Repo\Agg\IWarehouse */
+    protected $_repoWrhs;
     /** @var \Magento\Store\Model\Store */
-    private $_storeBalticEn;
+    protected $_storeBalticEn;
     /** @var \Magento\Store\Model\Store */
-    private $_storeBalticRu;
+    protected $_storeBalticRu;
     /** @var \Magento\Store\Model\Store */
-    private $_storeRussianRu;
+    protected $_storeRussianRu;
 
     public function __construct(
         \Magento\Framework\ObjectManagerInterface $manObj,
         \Praxigento\Core\Repo\ITransactionManager $manTrans,
         \Magento\Store\Api\GroupRepositoryInterface $mageRepoGroup,
-        \Magento\Store\Api\StoreRepositoryInterface $mageRepoStore
+        \Magento\Store\Api\StoreRepositoryInterface $mageRepoStore,
+        \Magento\CatalogInventory\Api\StockRepositoryInterface $mageRepoStock,
+        \Praxigento\Odoo\Repo\Agg\IWarehouse $repoWrhs
     ) {
         parent::__construct();
         $this->_manObj = $manObj;
         $this->_manTrans = $manTrans;
         $this->_mageRepoGroup = $mageRepoGroup;
         $this->_mageRepoStore = $mageRepoStore;
+        $this->_mageRepoStock = $mageRepoStock;
+        $this->_repoWrhs = $repoWrhs;
     }
 
     /**
@@ -91,6 +108,53 @@ class Stocks extends Command
     }
 
     /**
+     * Update default stock (switch to website #1 from website #0), add new stock and 2 warehouses; bind warehouses
+     * with stocks.
+     */
+    private function _processStocks()
+    {
+        /* update default stock as Baltic */
+        /** @var \Magento\CatalogInventory\Model\Stock $stockBaltic */
+        $stockBaltic = $this->_mageRepoStock->get(self::DEF_STOCK_ID_DEFAULT);
+        $stockBaltic->setStockName('Baltic');
+        $stockBaltic->setWebsiteId(self::DEF_WEBSITE_ID_MAIN);
+        $this->_mageRepoStock->save($stockBaltic);
+        /* create new stock as Russian */
+        try {
+            $stockRussian = $this->_mageRepoStock->get(self::DEF_STOCK_ID_RUSSIAN);
+        } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
+            $stockRussian = $this->_manObj->create(\Magento\CatalogInventory\Model\Stock::class);
+            // $stockRussian->setStockId(self::DEF_STOCK_ID_RUSSIAN);
+        } finally {
+            $stockRussian->setStockName('Russian');
+            $stockRussian->setWebsiteId(self::DEF_WEBSITE_ID_MAIN);
+            $this->_mageRepoStock->save($stockRussian);
+        }
+        /* create new warehouses */
+        /* Baltic */
+        $wrhsBaltic = $this->_repoWrhs->getById(self::DEF_STOCK_ID_BALTIC);
+        $wrhsBaltic->setCode('Baltic');
+        $wrhsBaltic->setCurrency('EUR');
+        $wrhsBaltic->setNote('Warehouse for Baltic states (LV, LT, EE)');
+        $wrhsBaltic->setOdooId(22);
+        $this->_repoWrhs->updateById($wrhsBaltic->getId(), $wrhsBaltic);
+        /* Russian */
+        $wrhsRussian = $this->_repoWrhs->getById(self::DEF_STOCK_ID_RUSSIAN);
+        $shoudCreate = is_null($wrhsRussian->getOdooId());
+        $wrhsRussian->setCode('Russian');
+        $wrhsRussian->setCurrency('RUB');
+        $wrhsRussian->setNote('Warehouse for Russian Federation');
+        $wrhsRussian->setOdooId(33);
+        if ($shoudCreate) {
+            $this->_repoWrhs->create($wrhsRussian);
+        } else {
+            $this->_repoWrhs->updateById($wrhsRussian->getId(), $wrhsRussian);
+        }
+        return;
+
+    }
+
+    /**
      * Update default store (store view) for Baltic group (as EN store) and create new stores for Balrics (as RU store)
      * and for Russian group (as RU store).
      */
@@ -103,7 +167,7 @@ class Stocks extends Command
         $this->_storeBalticEn->setCode('baltic_en');
         $this->_storeBalticEn->save();
         /* create RU-store for Baltic group */
-        $this->_storeBalticRu= $this->_manObj->create(\Magento\Store\Model\Store::class);
+        $this->_storeBalticRu = $this->_manObj->create(\Magento\Store\Model\Store::class);
         $this->_storeBalticRu->load(self::DEF_STORE_ID_BALTIC_RU);
         $this->_storeBalticRu->setName('RU');
         $this->_storeBalticRu->setCode('baltic_ru');
@@ -113,7 +177,7 @@ class Stocks extends Command
         $this->_storeBalticRu->setIsActive(true);
         $this->_storeBalticRu->save();
         /* create Ru-store for Russian group */
-        $this->_storeRussianRu= $this->_manObj->create(\Magento\Store\Model\Store::class);
+        $this->_storeRussianRu = $this->_manObj->create(\Magento\Store\Model\Store::class);
         $this->_storeRussianRu->load(self::DEF_STORE_ID_RUSSIAN_RU);
         $this->_storeRussianRu->setName('RU');
         $this->_storeRussianRu->setCode('russian_ru');
@@ -160,6 +224,7 @@ class Stocks extends Command
         try {
             $this->_processGroups();
             $this->_processStores();
+            $this->_processStocks();
             $this->_manTrans->transactionCommit($trans);
         } finally {
             // transaction will be rolled back if commit is not done (otherwise - do nothing)
