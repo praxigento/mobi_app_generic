be0125b5aef17bcf7f4524023da5941b61a00b0d
Alex Gusev <alex@flancer64.com>
2016-06-14 16:42:40 +0300

    MOBI-312 - Magento 2 test order invoice issue

--- src/Tool/Warehouse/Def/StockManager.php
+++ src/Tool/Warehouse/Def/StockManager.php
@@ -5,23 +5,21 @@
 
 namespace Praxigento\App\Generic2\Tool\Warehouse\Def;
 
-use \Praxigento\App\Generic2\Console\Command\Init\Stocks as InitStocks;
+use \Praxigento\App\Generic2\Console\Command\Init\Stocks as Init;
 
 class StockManager implements \Praxigento\Warehouse\Tool\IStockManager
 {
-    /**
-     * Expected values for stores & stocks IDs.
-     */
-    const DEF_STOCK_ID_BALTIC = InitStocks::DEF_STOCK_ID_BALTIC;
-    const DEF_STOCK_ID_RUSSIAN = InitStocks::DEF_STOCK_ID_RUSSIAN;
-    const DEF_GROUP_ID_BALTIC = InitStocks::DEF_GROUP_ID_BALTIC;
-    const DEF_GROUP_ID_RUSSIAN = InitStocks::DEF_GROUP_ID_RUSSIAN;
 
     /** @var  \Magento\Store\Model\StoreManagerInterface */
     protected $_manStore;
     protected $_mapGroupToStock = [
-        self::DEF_GROUP_ID_BALTIC => self::DEF_STOCK_ID_BALTIC,
-        self::DEF_GROUP_ID_RUSSIAN => self::DEF_STOCK_ID_RUSSIAN
+        Init::DEF_GROUP_ID_BALTIC => Init::DEF_STOCK_ID_BALTIC,
+        Init::DEF_GROUP_ID_RUSSIAN => Init::DEF_STOCK_ID_RUSSIAN
+    ];
+    protected $_mapStoreToStock = [
+        Init::DEF_STORE_ID_BALTIC_EN => Init::DEF_STOCK_ID_BALTIC,
+        Init::DEF_STORE_ID_BALTIC_RU => Init::DEF_STOCK_ID_BALTIC,
+        Init::DEF_STORE_ID_RUSSIAN_RU => Init::DEF_STOCK_ID_RUSSIAN
     ];
 
     public function __construct(
@@ -36,6 +34,14 @@ class StockManager implements \Praxigento\Warehouse\Tool\IStockManager
         $id = $group->getId();
         $result = isset($this->_mapGroupToStock[$id]) ?
             $this->_mapGroupToStock[$id] :
+            \Magento\CatalogInventory\Model\Stock::DEFAULT_STOCK_ID;
+        return $result;
+    }
+
+    public function getStockIdByStoreId($storeId)
+    {
+        $result = isset($this->_mapStoreToStock[$storeId]) ?
+            $this->_mapStoreToStock[$storeId] :
             \Magento\CatalogInventory\Model\Stock::DEFAULT_STOCK_ID;
         return $result;
     }
