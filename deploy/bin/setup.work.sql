8ebb9657d4c63624dbf356118dc93bcf854ea85b
Alex Gusev <alex@flancer64.com>
2016-06-14 19:04:12 +0300

    MOBI-313 - Magento 2 test card payment issue

--- deploy/bin/setup.work.sql
+++ deploy/bin/setup.work.sql
@@ -4,6 +4,7 @@
 REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = '1', path ='dev/template/allow_symlink';
 REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = '1', path ='dev/log/active';
 REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = 'America/Los_Angeles', path ='general/locale/timezone';
+REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = '0', path ='checkout/options/guest_checkout';
 -- setup Braintree payment gateway
 REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = '1', path ='payment/braintree/active';
 REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = 'sandbox', path ='payment/braintree/environment';
