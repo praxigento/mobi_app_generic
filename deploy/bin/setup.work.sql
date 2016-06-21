5bba49a9e601bb31274c7ef8cb80e45e61f85efe
Alex Gusev <alex@flancer64.com>
2016-06-21 11:37:01 +0300

    MOBI-319 - Add mgtcommerce/module-mgtdevelopertoolbar

--- deploy/bin/setup.work.sql
+++ deploy/bin/setup.work.sql
@@ -6,11 +6,13 @@ REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = '1', path ='dev/log/ac
 REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = 'America/Los_Angeles', path ='general/locale/timezone';
 REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = '36000', path ='admin/security/session_lifetime';
 REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = '100000', path ='cataloginventory/options/stock_threshold_qty';
+-- enable development toolbar
+REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = '1', path ='mgt_developer_toolbar/module/is_enabled';
 -- REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = '0', path ='checkout/options/guest_checkout';
 -- setup Braintree payment gateway
 REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = '1', path ='payment/braintree/active';
 REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = 'sandbox', path ='payment/braintree/environment';
-REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = 'authorize_capture', path ='payment/braintree/payment_action';
+REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = 'authorize_capture', path ='payment/braintree/payment_actish ./wo on';
 REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = 'hrbwv69nr663dxx6', path ='payment/braintree/merchant_id';
 REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = '0:2:MNCxt7WoXHyj5pNckGBT7QsW4Rvau5j7:SISqkD6irXdKO1hqJk+YSNZ0QndYJ8HEpGq+XoZ5LtY=', path ='payment/braintree/public_key';
 REPLACE INTO ${CFG_DB_PREFIX}core_config_data SET value = '0:2:Mu2JghxOFGKVkM6RdQOT8BOPWPaSY7rB:UZ+tVA7HvZgGhgdStzAjsr7qaQ6mWsStG8xedKn8mcY=', path ='payment/braintree/private_key';
