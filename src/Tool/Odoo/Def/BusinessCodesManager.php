60c8407c8d0a62f6b6080dfed7cf989f3ecd055a
Alex Gusev <alex@flancer64.com>
2016-07-15 13:09:53 +0300

    MOBI-348 - Business codes for Payment & Shipping methods.

--- src/Tool/Odoo/Def/BusinessCodesManager.php
+++ src/Tool/Odoo/Def/BusinessCodesManager.php
@@ -9,15 +9,33 @@ class BusinessCodesManager
     implements \Praxigento\Odoo\Tool\IBusinessCodesManager
 {
 
-    public function getShippingMethodCode(\Magento\Sales\Api\Data\OrderInterface $order)
-    {
-        $result = $order->getShippingMethod();
-        return $result;
-    }
+    const B_PAY_BRAINTREE = 'ccard_braintree';
+    const B_PAY_CHECK_MONEY = 'check_money';
+    const B_SHIP_FLAT_RATE = 'flat_rate';
+    const M_PAY_BRAINTREE = 'braintree';
+    const M_PAY_CHECK_MONEY = 'checkmo';
+    const M_SHIP_FLAT_RATE = 'flatrate_flatrate';
 
     public function getPaymentMethodCode(\Magento\Sales\Api\Data\OrderPaymentInterface $payment)
     {
-        $result = $payment->getMethod();
+        $result = null;
+        $mage = $payment->getMethod();
+        if ($mage == self::M_PAY_BRAINTREE) {
+            $result = self::B_PAY_BRAINTREE;
+        }
+        if ($mage == self::M_PAY_CHECK_MONEY) {
+            $result = self::B_PAY_CHECK_MONEY;
+        }
+        return $result;
+    }
+
+    public function getShippingMethodCode(\Magento\Sales\Api\Data\OrderInterface $order)
+    {
+        $result = null;
+        $mage = $order->getShippingMethod();
+        if ($mage == self::M_SHIP_FLAT_RATE) {
+            $result = self::B_SHIP_FLAT_RATE;
+        }
         return $result;
     }
 }
