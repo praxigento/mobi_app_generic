7cf90529def3164c6149beb66ef84c4b7e8ff153
Alex Gusev <alex@flancer64.com>
2016-07-25 11:28:11 +0300

    MOBI-326 - Add taxes and sales rules to initial data

---
+++ test/manual/Console/Command/Init/Sub/SalesRules_Test.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+namespace Praxigento\App\Generic2\Console\Command\Init\Sub;
+
+use Magento\Framework\App\ObjectManager;
+
+
+include_once(__DIR__ . '/../../../../phpunit_bootstrap.php');
+
+class SalesRules_ManualTest extends \Praxigento\Core\Test\BaseMockeryCase
+{
+    /** @var  \Praxigento\App\Generic2\Console\Command\Init\Sub\SalesRules */
+    private $_obj;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->_obj = ObjectManager::getInstance()->create(\Praxigento\App\Generic2\Console\Command\Init\Sub\SalesRules::class);
+    }
+
+    public function test_init()
+    {
+        $this->_obj->init();
+        return;
+    }
+
+}
