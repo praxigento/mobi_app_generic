12a77a9bd805ed58c349781e7659e593441aac05
Alex Gusev <alex@flancer64.com>
2016-09-02 15:13:09 +0300

    MOBI-427 - Add groups to test customers tree

--- src/Console/Command/Init/Customers.php
+++ src/Console/Command/Init/Customers.php
@@ -5,6 +5,7 @@
 
 namespace Praxigento\App\Generic2\Console\Command\Init;
 
+use Praxigento\App\Generic2\Tool\Odoo\Def\BusinessCodesManager;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -12,10 +13,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 class Customers
     extends \Symfony\Component\Console\Command\Command
 {
-    /**
-     * Downline tree default dependencies.
-     * @var array
-     */
+    /** Default downline tree */
     protected $DEFAULT_DWNL_TREE = [
         1 => 1,
         2 => 1,
@@ -31,6 +29,8 @@ class Customers
         12 => 10,
         13 => 10
     ];
+    /** Retail customers */
+    protected $GROUP_RETAIL = [9, 12];
     /** @var string 'UserPassword12 */
     protected $DEFAULT_PASSWORD_HASH = '387cf1ea04874290e8e3c92836e1c4b630c5abea110d8766bddb4b3a6224ea04:QVIfkMF7kfwRkkC3HdqJ84K1XANG38LF:1';
     /** @var \Magento\Framework\ObjectManagerInterface */
@@ -105,25 +105,29 @@ class Customers
         $this->_setAreaCode();
         $def = $this->_manTrans->begin();
         try {
-//            foreach ($this->DEFAULT_DWNL_TREE as $custId => $parentId) {
-//                $first = 'User' . $custId;
-//                $last = 'Last';
-//                $email = "customer_$custId@test.com";
-//                if ($custId != $parentId) {
-//                    /* save parent ID to registry */
-//                    $referralCode = $this->_mapCustomerMageIdByIndex[$parentId];
-//                    $this->_toolReferral->replaceCodeInRegistry($referralCode);
-//                }
-//                /** @var \Magento\Customer\Api\Data\CustomerInterface $customer */
-//                $customer = $this->_manObj->create(\Magento\Customer\Api\Data\CustomerInterface::class);
-//                $customer->setEmail($email);
-//                $customer->setFirstname($first);
-//                $customer->setLastname($last);
-//                /** @var \Magento\Customer\Api\Data\CustomerInterface $saved */
-//                $saved = $this->_repoMageCustomer->save($customer, $this->DEFAULT_PASSWORD_HASH);
-//                $this->_mapCustomerMageIdByIndex[$custId] = $saved->getId();
-//                $this->_mapCustomerIndexByMageId[$saved->getId()] = $custId;
-//            }
+            foreach ($this->DEFAULT_DWNL_TREE as $custId => $parentId) {
+                $first = 'User' . $custId;
+                $last = 'Last';
+                $email = "customer_$custId@test.com";
+                if ($custId != $parentId) {
+                    /* save parent ID to registry */
+                    $referralCode = $this->_mapCustomerMageIdByIndex[$parentId];
+                    $this->_toolReferral->replaceCodeInRegistry($referralCode);
+                }
+                /** @var \Magento\Customer\Api\Data\CustomerInterface $customer */
+                $customer = $this->_manObj->create(\Magento\Customer\Api\Data\CustomerInterface::class);
+                $customer->setEmail($email);
+                $customer->setFirstname($first);
+                $customer->setLastname($last);
+                /* MOBI-427: change group ID for retail customers */
+                if (in_array($custId, $this->GROUP_RETAIL)) {
+                    $customer->setGroupId(BusinessCodesManager::M_CUST_GROUP_RETAIL);
+                }
+                /** @var \Magento\Customer\Api\Data\CustomerInterface $saved */
+                $saved = $this->_repoMageCustomer->save($customer, $this->DEFAULT_PASSWORD_HASH);
+                $this->_mapCustomerMageIdByIndex[$custId] = $saved->getId();
+                $this->_mapCustomerIndexByMageId[$saved->getId()] = $custId;
+            }
             /* MOBI-426 : rename customer groups according to Generic App scheme. */
             $this->_subCustomerGroups->renameGroups();
             $this->_manTrans->commit($def);
