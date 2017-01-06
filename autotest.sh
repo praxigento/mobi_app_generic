e794e76f2b801357f05d185981363a2db5dee629
Alex Gusev <alex@flancer64.com>
2017-01-06 14:15:53 +0200

    MOBI-551 - Add script to launch integration test by cron

---
+++ autotest.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+## *************************************************************************
+#   Magento 2 auto deployment by cron.
+## *************************************************************************
+
+
+# pin current folder and deployment root folder
+CUR_DIR="$PWD"
+DIR_ROOT="$( cd "$( dirname "$0" )" && pwd )"
+
+echo ""
+echo "Go to test root and run all tests"
+cd ${DIR_ROOT}/work/theater
+/usr/bin/npm test
+
+# Finalize job
+echo ""
+echo "Auto deployment is done."
+cd ${CUR_DIR}
