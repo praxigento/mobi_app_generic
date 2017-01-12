1a21a71cb9b6e8f45af223f32988b20bf932045e
Alex Gusev <alex@flancer64.com>
2017-01-12 11:53:39 +0200

    MOBI-563 - Duplicate entry '8-0' for key 'M2_CATALOGINVENTORY_STOCK_ITEM_PRODUCT_ID_WEBSITE_ID'

--- deploy/bin/setup.work.sql
+++ deploy/bin/setup.work.sql
@@ -94,9 +94,9 @@ REPLACE INTO ${CFG_DB_PREFIX}tax_calculation(tax_calculation_id, tax_calculation
 --
 
 -- MOBI-254
-ALTER TABLE ${CFG_DB_PREFIX}cataloginventory_stock_item ADD UNIQUE INDEX CATALOGINVENTORY_STOCK_ITEM_PRODUCT_ID_STOCK_ID (product_id, stock_id);
-ALTER TABLE ${CFG_DB_PREFIX}cataloginventory_stock_item DROP INDEX CATALOGINVENTORY_STOCK_ITEM_PRODUCT_ID_WEBSITE_ID;
+ALTER TABLE ${CFG_DB_PREFIX}cataloginventory_stock_item ADD UNIQUE INDEX ${CFG_DB_PREFIX}CATALOGINVENTORY_STOCK_ITEM_PRODUCT_ID_STOCK_ID (product_id, stock_id);
+ALTER TABLE ${CFG_DB_PREFIX}cataloginventory_stock_item DROP INDEX ${CFG_DB_PREFIX}CATALOGINVENTORY_STOCK_ITEM_PRODUCT_ID_WEBSITE_ID;
 
 ALTER TABLE ${CFG_DB_PREFIX}cataloginventory_stock
-  ADD CONSTRAINT FK_cataloginventory_stock_store_website_website_id FOREIGN KEY (website_id)
+  ADD CONSTRAINT FK_${CFG_DB_PREFIX}cataloginventory_stock_store_website_website_id FOREIGN KEY (website_id)
   REFERENCES ${CFG_DB_PREFIX}store_website(website_id) ON DELETE RESTRICT ON UPDATE RESTRICT;
