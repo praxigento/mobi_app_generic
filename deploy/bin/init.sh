c91cf362dfcb4191b76143ae750adc96810149d8
Alex Gusev <alex@flancer64.com>
2017-07-04 13:20:59 +0300

    MOBI-721 - Tax & discount mismatch with the reference order

--- deploy/bin/init.sh
+++ deploy/bin/init.sh
@@ -32,8 +32,6 @@ echo "Init downline tree."
 php ${DIR_MAGE}/bin/magento prxgt:app:init:customers
 echo "Init stores & stocks."
 php ${DIR_MAGE}/bin/magento prxgt:app:init:stocks
-echo "Init stores & stocks."
-php ${DIR_MAGE}/bin/magento prxgt:app:init:stocks
 echo "Replicate Odoo products."
 php ${DIR_MAGE}/bin/magento prxgt:odoo:replicate:products
 echo "Post-replication routines."
