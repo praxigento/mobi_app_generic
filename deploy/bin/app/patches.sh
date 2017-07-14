2e167e0c2b916cf42f90312ec7c5384773a24c0d
Alex Gusev <alex@flancer64.com>
2017-07-14 18:41:43 +0300

    MOBI-816 - Enable API request logging for Generic & Santegra

--- deploy/bin/app/patches.sh
+++ deploy/bin/app/patches.sh
@@ -16,6 +16,7 @@ fi
 echo ""
 echo "Patching Magento 2 files..."
 patch ${DIR_MAGE}/app/code/Magento/CatalogSearch/etc/di.xml ${DIR_DEPLOY}/patch/PR-7696.patch
+patch ${DIR_MAGE}/app/bootstrap.php ${DIR_DEPLOY}/patch/PR-10245.patch
 
 echo "All patches are applied."
 echo ""
