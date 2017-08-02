7504ddd703dbe37c7bfbef02126a8c65aba7a437
Alex Gusev <alex@flancer64.com>
2017-08-02 16:22:56 +0300

    MOBI-715 - Add API operation to get data for Downline reports

--- deploy/bin/app/mage.sh
+++ deploy/bin/app/mage.sh
@@ -51,7 +51,7 @@ composer create-project -s dev --prefer-source --keep-vcs --no-install \
     --repository "{\"type\": \"vcs\",\"url\": \"https://github.com/magento/magento2\"}" \
     magento/magento2ce ${DIR_MAGE}
 cd ${DIR_MAGE}
-git checkout 2.2.0-RC1.5
+git checkout 2.2.0-RC1.6
 composer install
 
 
