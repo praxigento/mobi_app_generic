29d99d5134e51f0412bc3873073553f485209300
Alex Gusev <alex@flancer64.com>
2018-03-23 20:21:02 +0200

    MOBI-1226 Rename $repo to $dao

--- Tool/Warehouse/Def/StockManager.php
+++ Tool/Warehouse/Def/StockManager.php
@@ -9,31 +9,34 @@ use Praxigento\App\Generic2\Cli\Init\Stocks as Init;
 
 class StockManager implements \Praxigento\Warehouse\Api\Helper\Stock
 {
-
+    /** @var \Praxigento\Warehouse\Repo\Dao\Warehouse */
+    private $daoWrhs;
     /** @var  \Magento\Store\Model\StoreManagerInterface */
-    protected $_manStore;
-    protected $_mapGroupToStock = [
+    private $manStore;
+    private $mapGroupToStock = [
         Init::DEF_GROUP_ID_BALTIC => Init::DEF_STOCK_ID_BALTIC,
         Init::DEF_GROUP_ID_RUSSIAN => Init::DEF_STOCK_ID_RUSSIAN
     ];
-    protected $_mapStoreToStock = [
+    private $mapStoreToStock = [
         Init::DEF_STORE_ID_BALTIC_EN => Init::DEF_STOCK_ID_BALTIC,
         Init::DEF_STORE_ID_BALTIC_RU => Init::DEF_STOCK_ID_BALTIC,
         Init::DEF_STORE_ID_RUSSIAN_RU => Init::DEF_STOCK_ID_RUSSIAN
     ];
 
     public function __construct(
-        \Magento\Store\Model\StoreManagerInterface $storeManager
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Praxigento\Warehouse\Repo\Dao\Warehouse $daoWrhs
     ) {
-        $this->_manStore = $storeManager;
+        $this->manStore = $storeManager;
+        $this->daoWrhs = $daoWrhs;
     }
 
     public function getCurrentStockId()
     {
-        $group = $this->_manStore->getGroup();
+        $group = $this->manStore->getGroup();
         $id = $group->getId();
-        $result = isset($this->_mapGroupToStock[$id]) ?
-            $this->_mapGroupToStock[$id] :
+        $result = isset($this->mapGroupToStock[$id]) ?
+            $this->mapGroupToStock[$id] :
             \Magento\CatalogInventory\Model\Stock::DEFAULT_STOCK_ID;
         return $result;
     }
@@ -43,10 +46,18 @@ class StockManager implements \Praxigento\Warehouse\Api\Helper\Stock
         return Init::DEF_STOCK_ID_BALTIC;
     }
 
+    public function getStockCurrencyByStoreId($storeId)
+    {
+        $stockId = $this->getStockIdByStoreId($storeId);
+        $wrhs = $this->daoWrhs->getById($stockId);
+        $result = $wrhs->getCurrency();
+        return $result;
+    }
+
     public function getStockIdByStoreId($storeId)
     {
-        $result = isset($this->_mapStoreToStock[$storeId]) ?
-            $this->_mapStoreToStock[$storeId] :
+        $result = isset($this->mapStoreToStock[$storeId]) ?
+            $this->mapStoreToStock[$storeId] :
             \Magento\CatalogInventory\Model\Stock::DEFAULT_STOCK_ID;
         return $result;
     }
